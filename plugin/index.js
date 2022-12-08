dcd55eba485365b1b08a1420fbf0ce1437f82e2b
Mohammed Razaq <mohammed.razaq@prettylittlething.com>
2022-12-08 00:33:39 +0000

    commit v2

--- plugin/index.js
+++ plugin/index.js
@@ -1,4 +1,4 @@
 module.exports.rules = {
-  'prefer-logical': require('./lib/rules/prefer-logical'),
-  'no-interpolated-classes': require('./lib/rules/no-interpolated-classes')
+  "prefer-logical": require("./lib/rules/prefer-logical"),
+  "no-interpolated-classes": require("./lib/rules/no-interpolated-classes"),
 };
